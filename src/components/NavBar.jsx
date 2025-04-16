2f1bdf60c37229d8a9d3cac77298f861b0569122
haseeb-505 <hatahir87626@gmail.com>
2025-04-16 15:04:25 +0500

    refactor Navbar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,23 +1,19 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
-export class NavBar extends Component {
-    constructor(){
-        super();
-        this.state={
-            isMenuOpen: false
-        };
+const NavBar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+        // this.setState({ isMenuOpen: !this.state.isMenuOpen });
     };
 
-    toggleMenu = () => {
-        this.setState({ isMenuOpen: !this.state.isMenuOpen });
+    const closeMenu = () => {
+        setIsMenuOpen(false);
+        // this.setState({ isMenuOpen: false });
     };
 
-    closeMenu = () => {
-        this.setState({ isMenuOpen: false });
-    };
-
-  render() {
     return (
       <div className='fixed top-0 w-full z-50'>
         <nav className="bg-white border-gray-200 dark:bg-gray-900">
@@ -42,14 +38,14 @@ export class NavBar extends Component {
             </div>
             <input type="text" id="search-navbar" className="block w-full md:p-1 md:text-xs lg:p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..."/>
             </div>
-            <button onClick={this.toggleMenu} type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-search" aria-expanded={this.state.isMenuOpen ? 'true' : 'false'}>
+            <button onClick={toggleMenu} type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-search" aria-expanded={isMenuOpen ? 'true' : 'false'}>
                 <span className="sr-only">Open main menu</span>
                 <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
                 </svg>
             </button>
         </div>
-            <div className={`items-center justify-between w-full md:flex md:w-auto md:order-1 md:block max-h-[80vh] overflow-y-auto overflow-x-hidden ${this.state.isMenuOpen ? 'block' : 'hidden'}`} id="navbar-search">
+            <div className={`items-center justify-between w-full md:flex md:w-auto md:order-1 md:block max-h-[80vh] overflow-y-auto overflow-x-hidden ${isMenuOpen ? 'block' : 'hidden'}`} id="navbar-search">
             <div className="relative mt-3 md:hidden">
                 <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                 <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
@@ -60,25 +56,25 @@ export class NavBar extends Component {
             </div>
             <ul className="flex flex-col p-3 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:text-sm md:space-x-2 lg:space-x-5 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                 <li>
-                <Link to="/" onClick={this.closeMenu} className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" >General</Link>
+                <Link to="/" onClick={closeMenu} className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" >General</Link>
                 </li>
                 <li>
-                    <Link to="/business" onClick={this.closeMenu} className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" >Business</Link>
+                    <Link to="/business" onClick={closeMenu} className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" >Business</Link>
                 </li>
                 <li>
-                    <Link to="/entertainment" onClick={this.closeMenu} className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Entertainment</Link>
+                    <Link to="/entertainment" onClick={closeMenu} className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Entertainment</Link>
                 </li>
                 <li>
-                    <Link to="/health" onClick={this.closeMenu} className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Health</Link>
+                    <Link to="/health" onClick={closeMenu} className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Health</Link>
                 </li>
                 <li>
-                    <Link to="/science" onClick={this.closeMenu} className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Science</Link>
+                    <Link to="/science" onClick={closeMenu} className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Science</Link>
                 </li>
                 <li>
-                    <Link to="/sports" onClick={this.closeMenu} className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Sports</Link>
+                    <Link to="/sports" onClick={closeMenu} className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Sports</Link>
                 </li>
                 <li>
-                    <Link to="/technology" onClick={this.closeMenu} className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Technology</Link>
+                    <Link to="/technology" onClick={closeMenu} className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Technology</Link>
                 </li>
                 
             </ul>
@@ -87,7 +83,6 @@ export class NavBar extends Component {
         </nav>
       </div>
     )
-  }
 }
 
-export default NavBar
+export default NavBar;
